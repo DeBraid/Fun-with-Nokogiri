7aae751e51fbd08180395bfb91ac5f10b89a64cd
DeBraid <de.braid@gmail.com>
2013-08-29 17:35:45 -0400

    images display with tickers

--- etf/etf.rb
+++ etf/etf.rb
@@ -3,27 +3,57 @@ require 'nokogiri'
 require 'open-uri'
 # key lies below
 
+def tickers # need a list of stock tickers
+
+end
+
+def grab_ticker(headline)
+  i = headline.chomp('-TSX)')
+  names_and_tickers = i.split('(')
+  names_and_tickers.last
+end
+
+# use the tickers to fetch the corresponding charts
+
+def get_chart_url(ticker)
+  "http://stockcharts.com/c-sc/sc?chart=#{ticker}.to,uu[305,a]dacayaci[pb20!b50][dg]"
+end
+
 page = Nokogiri::HTML(open("http://etf.stock-encyclopedia.com/category/etfs-listed-in-canada.html"))
 etfs = page.css('td.main table a')
 
 File.open('etflist.html', 'w') do |f| # f is rep the file with File.open
 
-f.puts("<html>")
-f.puts("<head>")
-f.puts("<title>Intro to HTML</title>")
-f.puts("</head>")
-f.puts("<body>")
-f.puts("<h1> List of ETF's in Canada</h1>")
-f.puts("<ul>")
+  f.puts("<html>")
+  f.puts("<head>")
+  f.puts("<title>Intro to HTML</title>")
+  f.puts("</head>")
+  f.puts("<body>")
+  f.puts("<h1> List of ETF's in Canada</h1>")
+  f.puts("<ul>")
 
-etfs.each do |etf|
-	# .text is a nokogiri method that is being called on etf
-	f.puts("   <li>" + etf.text + "</li>")
+  etfs.each do |etf|
+    ticker = grab_ticker(etf.text)
+  	f.puts("   <li>" + grab_ticker(etf.text) + "</li>")
+    f.puts("   <li>" + "<img src=\"#{get_chart_url(ticker)}\" />"  + "</li>")
+  end
+
+  f.puts ("</ul>")
+  f.puts ("</body>\n")
+  f.puts ("</html>\n")
+
 end
 
-f.puts ("</ul>")
-f.puts ("</body>\n")
-f.puts ("</html>\n")
+# To do: add
 
-# the nottation \n creates a new line (rather than printing all HTML on the same line.)
-end
+
+
+
+
+
+
+
+
+
+
+
